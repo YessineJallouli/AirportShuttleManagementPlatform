b13b330022f48eeb94cdc45d368fa65fb6bdef3c
YessineJallouli <yessinejallouli08@gmail.com>
2024-04-19 20:54:26 +0100

    adding dependencies

--- App.js
+++ App.js
@@ -12,16 +12,14 @@ const App = () => {
                 <SignUpScreen/>
             </SafeAreaView>
         </PaperProvider>
-        
+
     );
 }
 
 const styles = StyleSheet.create({
     container : {
-       flex : 1
+        flex : 1
     }
 });
 
 export default App;
-
-
