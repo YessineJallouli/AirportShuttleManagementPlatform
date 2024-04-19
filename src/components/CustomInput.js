aad2d7b3b14530da3fc72d093f900ad3c0cfe145
Mohamed Heni <mohamed.heni@ucar.insat.tn>
2024-04-19 22:13:46 +0200

    Revert "input validation"
    This reverts commit 26d4766b0a9e15f6eb56fa838e374baa1d8488f5.

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {View ,StyleSheet} from 'react-native';
 import { TextInput } from 'react-native-paper';
 const CustomInput = (props) => {
@@ -7,7 +8,6 @@ const CustomInput = (props) => {
             label = {props.name}
             mode = 'outlined'
             secureTextEntry= {props.secure}
-            onChangeText = {props.setValue}
         />
         </View>
         
